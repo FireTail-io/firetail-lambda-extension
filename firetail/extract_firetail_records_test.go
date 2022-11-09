ea2ce1f55d07a7ebc4ea02bd1f348f29636c07dd
Joshua O'Sullivan <joshua.r.osullivan@gmail.com>
2022-11-09 14:04:19 +0000

    Add test for happy path decodeFiretailRecord

---
+++ firetail/extract_firetail_records_test.go
@@ -0,0 +1,27 @@
+package firetail
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDecodeFiretailRecordHappy(t *testing.T) {
+	testRecord := Record{
+		Event:    events.APIGatewayProxyRequest{},
+		Response: RecordResponse{},
+	}
+	testPayloadBytes, err := json.Marshal(testRecord)
+	require.Nil(t, err)
+
+	encodedRecord := "firetail:test-token:" + base64.StdEncoding.EncodeToString(testPayloadBytes)
+
+	decodedRecord, err := decodeFiretailRecord(encodedRecord, "test-token")
+	require.Nil(t, err)
+
+	assert.Equal(t, *decodedRecord, testRecord)
+}
