fb7c512f70945929ce972207b8933eb7095e69b9
Joshua O'Sullivan <joshua@firetail.io>
2022-11-17 14:43:53 +0000

    Test decodeFiretailRecord

--- firetail/extract_firetail_records_test.go
+++ firetail/extract_firetail_records_test.go
@@ -1,6 +1,7 @@
 package firetail
 
 import (
+	"encoding/base32"
 	"encoding/base64"
 	"encoding/json"
 	"testing"
@@ -25,4 +26,113 @@ func TestDecodeFiretailRecordResponse(t *testing.T) {
 	require.Nil(t, err)
 
 	assert.Equal(t, testRecord.Response, *&decodedRecord.Response)
+}
+
+func TestDecodeFiretailRecordWithMissingPart(t *testing.T) {
+	encodedRecord := "firetail:log-ext"
+
+	decodedRecord, err := decodeFiretailRecord(encodedRecord)
+	assert.Nil(t, decodedRecord)
+	require.NotNil(t, err)
+
+	assert.Equal(t, "record had 2 parts when split by ':'", err.Error())
+}
+
+func TestDecodeFiretailRecordWithExtraPart(t *testing.T) {
+	testRecord := Record{
+		Response: RecordResponse{
+			StatusCode: 200,
+			Body:       "Test Body",
+		},
+	}
+	testPayloadBytes, err := json.Marshal(testRecord)
+	require.Nil(t, err)
+
+	encodedRecord := "firetail:log-ext:" + base64.StdEncoding.EncodeToString(testPayloadBytes) + ":extra"
+
+	decodedRecord, err := decodeFiretailRecord(encodedRecord)
+	assert.Nil(t, decodedRecord)
+	require.NotNil(t, err)
+
+	assert.Equal(t, "record had 4 parts when split by ':'", err.Error())
+}
+
+func TestDecodeFiretailRecordWithInvalidPrefix(t *testing.T) {
+	testRecord := Record{
+		Response: RecordResponse{
+			StatusCode: 200,
+			Body:       "Test Body",
+		},
+	}
+	testPayloadBytes, err := json.Marshal(testRecord)
+	require.Nil(t, err)
+
+	encodedRecord := "tailfire:log-ext:" + base64.StdEncoding.EncodeToString(testPayloadBytes)
+
+	decodedRecord, err := decodeFiretailRecord(encodedRecord)
+	assert.Nil(t, decodedRecord)
+	require.NotNil(t, err)
+
+	assert.Equal(t, "record did not have firetail prefix", err.Error())
+}
+
+func TestDecodeFiretailRecordWithInvalidToken(t *testing.T) {
+	testRecord := Record{
+		Response: RecordResponse{
+			StatusCode: 200,
+			Body:       "Test Body",
+		},
+	}
+	testPayloadBytes, err := json.Marshal(testRecord)
+	require.Nil(t, err)
+
+	encodedRecord := "firetail:ext-log:" + base64.StdEncoding.EncodeToString(testPayloadBytes)
+
+	decodedRecord, err := decodeFiretailRecord(encodedRecord)
+	assert.Nil(t, decodedRecord)
+	require.NotNil(t, err)
+
+	assert.Equal(t, "firetail prefixed record did not have valid token", err.Error())
+}
+
+func TestDecodeFiretailRecordWithInvalidPayloadEncoding(t *testing.T) {
+	testRecord := Record{
+		Response: RecordResponse{
+			StatusCode: 200,
+			Body:       "Test Body",
+		},
+	}
+	testPayloadBytes, err := json.Marshal(testRecord)
+	require.Nil(t, err)
+
+	encodedRecord := "firetail:log-ext:" + base32.StdEncoding.EncodeToString(testPayloadBytes)
+
+	decodedRecord, err := decodeFiretailRecord(encodedRecord)
+	assert.Nil(t, decodedRecord)
+	require.NotNil(t, err)
+
+	assert.Contains(t, err.Error(), "failed to b64 decode firetail record")
+}
+
+func TestDecodeFiretailRecordWithInvalidPayloadTypes(t *testing.T) {
+	type Event struct {
+		Event         string `json:"event"`
+		Response      int    `json:"response"`
+		ExecutionTime string `json:"execution_time"`
+	}
+	testRecord := Event{
+		Event:         "my birthday party",
+		Response:      0,
+		ExecutionTime: "tomorrow",
+	}
+	testPayloadBytes, err := json.Marshal(testRecord)
+	require.Nil(t, err)
+
+	encodedRecord := "firetail:log-ext:" + base64.StdEncoding.EncodeToString(testPayloadBytes)
+
+	decodedRecord, err := decodeFiretailRecord(encodedRecord)
+	assert.Nil(t, decodedRecord)
+	require.NotNil(t, err)
+
+	assert.Contains(t, err.Error(), "failed to unmarshal firetail event")
 }
