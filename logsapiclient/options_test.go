8323ebb408b0998600434a3bb03aed64b13903d2
Joshua O'Sullivan <joshua@firetail.io>
2022-11-23 15:50:27 +0000

    Test logs API client options

---
+++ logsapiclient/options_test.go
@@ -0,0 +1,31 @@
+package logsapiclient
+
+import (
+	"bytes"
+	"log"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSetDefaults(t *testing.T) {
+	testOptions := Options{}
+	testOptions.setDefaults()
+	assert.Equal(t, &defaultErrCallback, testOptions.ErrCallback)
+}
+
+func TestDefaultErrCallba(t *testing.T) {
+	testErr := errors.New("Test Error")
+
+	logBuffer := bytes.Buffer{}
+	log.SetOutput(&logBuffer)
+
+	defaultErrCallback(testErr)
+
+	logOutput, err := logBuffer.ReadString('\n')
+	require.Nil(t, err)
+
+	assert.Contains(t, logOutput, testErr.Error())
+}
