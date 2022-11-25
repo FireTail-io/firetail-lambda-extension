82a74a5348d16a9749348cf695fde4749eb03f85
Joshua O'Sullivan <joshua@firetail.io>
2022-11-25 08:08:57 +0000

    Add notice log when started in debug mode

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ func main() {
 	if isDebug, err := strconv.ParseBool(os.Getenv("FIRETAIL_EXTENSION_DEBUG")); err != nil || !isDebug {
 		// If we're not in debug mode, we'll just send the logs to the void
 		log.SetOutput(ioutil.Discard)
+	} else {
+		log.Println("Firetail extension starting in debug mode.")
 	}
 
 	// This context will be cancelled whenever a SIGTERM or SIGINT signal is received
