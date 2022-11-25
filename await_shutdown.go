77d256794d798563611e24a26d1c107c5eb0d0eb
Joshua O'Sullivan <joshua@firetail.io>
2022-11-25 08:09:23 +0000

    Don't log in awaitShutdown

--- await_shutdown.go
+++ await_shutdown.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"firetail-lambda-extension/extensionsapi"
-	"log"
 
 	"github.com/pkg/errors"
 )
@@ -14,10 +13,8 @@ func awaitShutdown(extensionClient *extensionsapi.Client, ctx context.Context) (
 	for {
 		select {
 		case <-ctx.Done():
-			log.Printf(", returning...")
 			return "context cancelled", nil
 		default:
-			log.Printf("Waiting for event...")
 			res, err := extensionClient.NextEvent(ctx) // This is a blocking call
 			if err != nil {
 				return "", errors.WithMessage(err, "failed to get next event")
