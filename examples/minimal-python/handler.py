d4195673e6666623740f4af317b5974a78eb76f3
theteacat <joshua@firetail.io>
2023-03-23 13:14:26 +0000

    Add Current-Time header to example

--- examples/minimal-python/handler.py
+++ examples/minimal-python/handler.py
@@ -11,9 +11,13 @@ app = firetail_app()
 
 @firetail_handler(app)
 def endpoint(event, context):
+    current_time = datetime.datetime.now().time()
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello, the current time is %s" % datetime.datetime.now().time()
-        })
+            "message": "Hello, the current time is %s" % current_time
+        }),
+        "headers": {
+            "Current-Time": "%s" % current_time
+        }
     }
