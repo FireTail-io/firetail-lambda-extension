810f9670de4b57819cda2a19c58bcd52c6ca0f4d
theteacat <joshua@firetail.io>
2023-03-23 13:12:14 +0000

    Remove requests and xray

--- examples/minimal-python/handler.py
+++ examples/minimal-python/handler.py
@@ -2,14 +2,10 @@ import sys, base64, datetime, json, os, time
 
 # Deps in src/vendor
 sys.path.insert(0, 'src/vendor')
-import requests
 
 from firetail_lambda import firetail_handler, firetail_app
 app = firetail_app()
 
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all
-patch_all()
 
 @firetail_handler(app)
 def endpoint(event, context):
