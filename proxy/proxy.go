8c18b496d0e0d8894cd08648d5a6c46c86d8066c
theteacat <joshua@firetail.io>
2025-05-14 16:55:13 +0100

    implement record assembler for proxy package

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,11 +1,17 @@
 package proxy
 
 import (
+	"context"
+	"encoding/json"
+	"firetail-lambda-extension/firetail"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -14,8 +20,9 @@ type ProxyServer struct {
 	runtimeEndpoint       string
 	port                  int
 	server                *http.Server
-	EventsChannel         chan *http.Response
-	LambdaResponseChannel chan *http.Request
+	eventsChannel         chan *http.Response
+	lambdaResponseChannel chan *http.Request
+	RecordsChannel        chan firetail.Record
 }
 
 func NewProxyServer() (*ProxyServer, error) {
@@ -29,8 +36,8 @@ func NewProxyServer() (*ProxyServer, error) {
 	ps := &ProxyServer{
 		runtimeEndpoint:       os.Getenv("AWS_LAMBDA_RUNTIME_API"),
 		port:                  port,
-		EventsChannel:         make(chan *http.Response),
-		LambdaResponseChannel: make(chan *http.Request),
+		eventsChannel:         make(chan *http.Response),
+		lambdaResponseChannel: make(chan *http.Request),
 	}
 
 	r := chi.NewRouter()
@@ -88,7 +95,7 @@ func NewProxyServer() (*ProxyServer, error) {
 			return nextEndpoint, nil
 		},
 		nil,
-		&ps.EventsChannel,
+		&ps.eventsChannel,
 	)
 	r.Get("/2018-06-01/runtime/invocation/next", nextHandler)
 
@@ -102,7 +109,7 @@ func NewProxyServer() (*ProxyServer, error) {
 				),
 			)
 		},
-		&ps.LambdaResponseChannel,
+		&ps.lambdaResponseChannel,
 		nil,
 	)
 	r.Post("/2018-06-01/runtime/invocation/{requestId}/response", responseHandler)
@@ -115,6 +122,62 @@ func NewProxyServer() (*ProxyServer, error) {
 	return ps, nil
 }
 
+func (p *ProxyServer) recordAssembler() {
+	for {
+		// Events and lambda responses should come in pairs, event first and response second.
+		event, ok := <-p.eventsChannel
+		if !ok {
+			log.Println("Events channel closed, stopping record assembler.")
+			return
+		}
+
+		// We can record the time between receiving the event and the response
+		// to calculate the execution time of the lambda function.
+		eventReceivedAt := time.Now()
+
+		lambdaResponse, ok := <-p.lambdaResponseChannel
+		if !ok {
+			log.Println("Lambda response channel closed, stopping record assembler.")
+			return
+		}
+
+		executionTime := time.Since(eventReceivedAt)
+
+		eventBody, err := io.ReadAll(event.Body)
+		if err != nil {
+			log.Println("Error reading event body:", err)
+			continue
+		}
+		responseBody, err := io.ReadAll(lambdaResponse.Body)
+		if err != nil {
+			log.Println("Error reading response body:", err)
+			continue
+		}
+
+		var recordResponse firetail.RecordResponse
+		if err := json.Unmarshal(responseBody, &recordResponse); err != nil {
+			log.Println("Error unmarshalling response body:", err)
+			continue
+		}
+
+		p.RecordsChannel <- firetail.Record{
+			Event:         eventBody,
+			Response:      recordResponse,
+			ExecutionTime: executionTime.Seconds(),
+		}
+	}
+}
+
 func (p *ProxyServer) ListenAndServe() error {
+	go p.recordAssembler()
 	return p.server.ListenAndServe()
+}
+
+func (p *ProxyServer) Shutdown(ctx context.Context) error {
+	if err := p.server.Shutdown(ctx); err != nil {
+		return err
+	}
+	close(p.eventsChannel)
+	close(p.lambdaResponseChannel)
+	return nil
 }
