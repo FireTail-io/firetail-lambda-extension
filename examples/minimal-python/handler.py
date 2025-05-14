7bf345509b30bd7dc22c052a8d80dfdea040c920
theteacat <joshua@firetail.io>
2025-05-14 14:40:32 +0100

    remove firetail-lambda python lib from example

--- examples/minimal-python/handler.py
+++ examples/minimal-python/handler.py
@@ -3,21 +3,13 @@ import json
 import sys
 
 # Deps in src/vendor
-sys.path.insert(0, 'src/vendor')
-
-from firetail_lambda import firetail_handler, firetail_app  # noqa: E402
-app = firetail_app()
+sys.path.insert(0, "src/vendor")
 
 
-@firetail_handler(app)
 def endpoint(event, context):
     current_time = datetime.datetime.now().time()
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "Hello, the current time is %s" % current_time
-        }),
-        "headers": {
-            "Current-Time": "%s" % current_time
-        }
+        "body": json.dumps({"message": "Hello, the current time is %s" % current_time}),
+        "headers": {"Current-Time": "%s" % current_time},
     }
